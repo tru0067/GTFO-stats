e2c8b6640ea7d58b93c7cb55815dad19b4e2684c
tru0067@cersonal-pomputer <33011616+tru0067@users.noreply.github.com>
2024-04-04 02:01:54 +1100

    Add Zoomer Scout

--- enemies.jl
+++ enemies.jl
@@ -26,7 +26,7 @@ const NIGHTMARE_SHOOTER = Enemy("Nightmare Shooter",   18,   3, 2,    8,   18, "
 const BABY              = Enemy("Baby",                 5,   2, 2,    5,    2, "N/A",  "4%",        "N/A")
 const FLYER             = Enemy("Flyer",             16.2,   3, 2,    4, 16.2, "N/A", "N/A",     "2 × 4%")
 const SCOUT             = Enemy("Scout",               42,   3, 2,    5,   42, "N/A", "N/A",     "3 × 4%")
-const CSCOUT            = Enemy("Charger Scout",       60,   1, 2,    5,   60, "12%", "N/A",        "N/A")
+const ZOOMER_SCOUT      = Enemy("Zoomer Scout",        33,   3, 2,    5,   33, "N/A", "N/A",     "3 × 4%")
 const SNATCHER          = Enemy("Snatcher",           225,   1, 2,   40,  225, "N/A", "17%",        "N/A")
 const MOTHER            = Enemy("Mother",            1000,   5, 1, 1000,  126, "N/A", "N/A",        "N/A")
 const PMOTHER           = Enemy("pMother",           2500,   5, 1, 2500,  313, "N/A", "N/A",        "N/A")
@@ -34,5 +34,5 @@ const TANK              = Enemy("Tank",              1000,   3, 1,  400,  150, "
 
 const ENEMIES = (
     STRIKER, SHOOTER, GIANT, BIG_SHOOTER, HYBRID, SHADOW, CHARGER, NIGHTMARE_STRIKER, NIGHTMARE_SHOOTER,
-    BABY, FLYER, SCOUT, CSCOUT, SNATCHER, MOTHER, PMOTHER, TANK,
+    BABY, FLYER, SCOUT, ZOOMER_SCOUT, CSCOUT, SNATCHER, MOTHER, PMOTHER, TANK,
 )
